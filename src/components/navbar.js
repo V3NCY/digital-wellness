538f5a684bb9daba2b3b4e5cb99228819e88ab59
unknown <venceslavam@gmail.com>
2023-08-24 11:01:11 +0300

    new navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,25 +3,37 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 
 
-function ColorSchemesExample() {
+function BasicExample() {
     return (
-        <>
-            <Navbar bg="dark" data-bs-theme="dark">
-                <Container>
-                    <Navbar.Brand href="/"><img src="logo.png" alt="Girl in a jacket" width="90" height="90" /></Navbar.Brand>
+        <Navbar expand="lg" className="bg-dark navbar-dark">
+            <Container>
+                <Navbar.Brand href="/"><img src="logo.png" alt="Girl in a jacket" width="90" height="90" /></Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="/">Dashboard</Nav.Link>
-                        <Nav.Link href="/activity">Activity Tracking</Nav.Link>
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href="/activity">Activity</Nav.Link>
                         <Nav.Link href="/homeworks">Homeworks</Nav.Link>
-                        <Nav.Link href="/settings">Settings</Nav.Link>
+                        <NavDropdown title="Settings" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="/settings">Settings</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">
+                                Another action
+                            </NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">
+                                Separated link
+                            </NavDropdown.Item>
+                        </NavDropdown>
                     </Nav>
-                </Container>
-            </Navbar>
-        </>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 }
 
-export default ColorSchemesExample;
+export default BasicExample;
